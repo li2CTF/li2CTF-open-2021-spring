29f5d2d75b110387d1832902aa3b30db5985b2d1
sultanowskii <dsr431@yandex.ru>
2021-04-02 17:57:45 +0300

    Maths task added

---
+++ junior/ppc/Maths/solution/hints.md
@@ -0,0 +1,8 @@
+# Hints
+## Please don't do it with your hands
+Use `socket` python library
+
+---
+
+## You must answer for 50 questions
+And then you will get flag
---
+++ junior/ppc/Maths/solution/solver.py
@@ -0,0 +1,25 @@
+import socket
+
+CORRECT_LINE = b'Correct\n'
+READY_LINE = b"Ready?\n"
+
+sock = socket.socket()
+sock.connect(("0.0.0.0", 23001))
+
+
+while True:
+    data = sock.recv(1024)
+    if data.endswith(CORRECT_LINE) or data.endswith(READY_LINE):
+        data = sock.recv(1024)
+    else:
+        sep = CORRECT_LINE
+        if READY_LINE in data:
+            sep = READY_LINE
+        data = data[(data.index(sep) + len(sep)) if sep in data else 0:]
+    try:
+        a, b = [int(c) for c in data.decode("ascii").strip().split(", ")]
+    except Exception:
+        # it's exactly the flag
+        print(data.decode())
+        break
+    sock.send(f"{a * b}".encode("ascii"))
